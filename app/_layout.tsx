5beef01fe5fbbe9777afd239223e71b23cf86ad6
songlin81 <songlin81@gmail.com>
2024-10-19 12:51:29 +0000

    loading corrected

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
 
         // Artificially delay for two seconds to simulate a slow loading
         // experience. Please remove this if you copy and paste the code!
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        await new Promise(resolve => setTimeout(resolve, 4000));
       } catch (e) {
         console.warn(e);
       } finally {
